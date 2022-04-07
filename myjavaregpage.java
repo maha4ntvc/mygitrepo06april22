96285eeb69abdc6ee7b630e49bbc79a6418ef858
FIRST_NAME LAST_NAME <MY_NAME@example.com>
2022-04-07 18:30:00 +0530

    my task completed

--- myjavaregpage.java
+++ myjavaregpage.java
@@ -1,2 +1,7 @@
 this is my reg page
 this is my my manager work completed
+1st day of java line of code 
+2nd day of java line of code
+3rd day of java line of code
+4th day of java line of code 
+5th day of java line of code 
